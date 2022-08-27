a32a2b74e545f2220bb4988b2db005e070df0803
Niko Kamtsioras <ckamtsioras@gmail.com>
2022-08-27 15:36:06 +0300

    add cpu temp

--- prometheus_client_test.py
+++ prometheus_client_test.py
@@ -7,7 +7,7 @@ RAM = prom.Gauge(so.gethostname() + "_memory_used_precent", '')
 CPU = prom.Gauge(so.gethostname() + "_cpu_used_precent", '')
 total_ram = prom.Gauge(so.gethostname() + "_total_ram", 'pc total ram')
 used_ram = prom.Gauge(so.gethostname() + "_used_ram", 'pc used ram')
-
+CPU_Temp = prom.Gauge(so.gethostname() + "_cpu_temp", 'temp')
 
 def get_ram():
     ramu = psutil.virtual_memory()[2]
@@ -29,13 +29,25 @@ def get_cpu():
     cpul = sum(cpuu) / len(cpuu)
     CPU.set(cpul)
 
+def get_temp():
+    temd = psutil.sensors_temperatures()
+    teml = (temd['cpu_thermal'])
+    temls = str(teml)
+    temls1 = temls.replace("(",'')
+    temls2 = temls1.replace(")",'')
+    temls3 = temls2.replace("[shwtemplabel='', current=",'')
+    temp = temls3.replace(", high=None, critical=None]",'')
+    CPU_Temp.set(temp)
+
 
 def Main():
+    
     while True:
         get_ram()
         get_total_ram()
         get_used_ram()
         get_cpu()
+        get_temp()
         t.sleep(1)
 
 
